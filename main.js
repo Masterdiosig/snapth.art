b340cba5684a4825d7590695df0e888f0ac0f162
Trung <trunghieue@gmail.com>
2025-08-02 18:04:20 +0300

    Cập nhật giao diện landing page

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector(".simple_form");
   const input = document.getElementById("hf_urli");
 
+
+
   // Hàm hiện lỗi ngay dưới ô nhập
   function showErrorInline(message) {
     const box = document.getElementById("error-inline");
@@ -13,36 +15,38 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 4000);
   }
 
+
   form.addEventListener("submit", async (e) => {
-    e.preventDefault(); // ✅ Chặn reload mặc định
+  e.preventDefault();
 
-    const tiktokUrl = input.value.trim();
-    if (!tiktokUrl) {
-      showErrorInline("Paste valid link!");
-       input.focus();
-      return;
-    }
+  const tiktokUrl = input.value.trim();
+  if (!tiktokUrl) {
+    showErrorInline("Paste valid link!");
+    input.focus();
+    return;
+  }
 
-    try {
-      const res = await fetch('/api/tiktok', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ url: tiktokUrl })
-      });
+  try {
+    const res = await fetch('/api/tiktok', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer my_super_secret_token_123' // ✅ đưa vào đúng chỗ
+      },
+      body: JSON.stringify({ url: tiktokUrl })
+    });
 
-      const data = await res.json();
+    const data = await res.json();
 
-      if (data.code === 0 && data.data.length > 0) {
-        window.open(data.data[0].url, '_blank');
-      } else {
-        showErrorInline("Error video or server.");
-
-
-      }
-    } catch (err) {
-      console.error(err);
+    if (data.code === 0 && data.data.length > 0) {
+      window.open(data.data[0].url, '_blank');
+    } else {
       showErrorInline("Error video or server.");
     }
-  });
+  } catch (err) {
+    console.error(err);
+    showErrorInline("Error video or server.");
+  }
 });
 
+});
