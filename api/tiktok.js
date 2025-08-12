fa6cc8b077b04f197cb5b6909c8347f0eae34bdd
Trung <trunghieue@gmail.com>
2025-08-12 15:45:57 +0300

    Cập nhật giao diện landing page

--- api/tiktok.js
+++ api/tiktok.js
@@ -56,7 +56,7 @@ const handler = async (req, res) => {
   console.log("🔗 Final TikTok URL:", finalUrl);
 
   try {
-    const response = await axios.get('https://tiktok-video-downloader-api.p.rapidapi.com/media', {
+    const response = await axios.get('https://tiktok-download-video1.p.rapidapi.com/newGetVideo', {
    params: {
   url: finalUrl,
   hd: '1'
@@ -64,7 +64,7 @@ const handler = async (req, res) => {
 
       headers: {
         'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
-        'X-RapidAPI-Host': 'tiktok-video-downloader-api.p.rapidapi.com'
+        'X-RapidAPI-Host': 'tiktok-download-video1.p.rapidapi.com'
       }
     });
 
